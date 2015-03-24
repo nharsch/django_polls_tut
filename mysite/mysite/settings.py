6f17e2d8cdfd14a54a3f13abefc2f40dd91756ed
nharsch <nigelharsch@gmail.com>
2015-03-24 12:27:40 -0400

    stashed scret key in env

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^1*w3!k^-1_4jh^&=$7rjct+hc36$k!so8=+2i86-xi*lm-jrp'
+SECRET_KEY = os.environ['DJANGO_TUT_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
