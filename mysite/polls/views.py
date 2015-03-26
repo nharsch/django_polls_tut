d2d14cff95e486a93d9758f91dad50f097729bb7
nharsch <nigelharsch@gmail.com>
2015-03-25 23:52:05 -0400

    got get object or 404 to work

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import RequestContext, loader
+from django.shortcuts import get_object_or_404
 
 from polls.models import Question
 
@@ -15,7 +16,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
