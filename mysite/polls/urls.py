16943832bf3ae912ba890bc3d04234599675dfba
nharsch <nigelharsch@gmail.com>
2015-03-25 23:42:15 -0400

    fixed regex patterns

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,9 +4,9 @@ from polls import views
 
 urlpatterns = patterns('',
     # ex: /polls/
-    url(r'^', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     # ex: /polls/5/
-    url(r'^?P<question_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
     # ex: /polls/5/results
     url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
     # ex : polls/5/vote/
